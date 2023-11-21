adc30f9babdd344e6d605a8ae00d51af9a3b84b9
NafeelJavaid <147493305+NafeelJavaid@users.noreply.github.com>
2023-11-21 11:07:27 +0500

    Add files via upload

---
+++ lab 6 h-3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x=1;
+	for(int i=0;i<4;i++){
+		if(i>=1 && i<=2)
+		{ x*=2;
+		}
+		else if(i==3)
+		{x=x+2;
+		}
+		else
+		{
+		}
+	 for(int j=0;j<x;j++)
+	 
+	 {
+	 	cout<<x<<" ";
+	 }
+	 cout<<endl;
+	}
+	
+}
